00daf75bdb2ac796057777d4646bba6478b42971
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2025-01-09 10:44:26 +0530

    updated trial.py

--- trial.py
+++ trial.py
@@ -1,51 +1,54 @@
-def setZeroes(matrix):
+"""module for STTCSE LAB 1"""
+
+def set_zeroes(matrix):
+    """funtion to set the rows and columns zero if the matrix has zero there"""
     foundzero = 0
     zi=-1
     zj=-1
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if (matrix[i][j] == 0):
+    for i, mi in enumerate(matrix):
+        for j, mj in enumerate(mi):
+            if mj == 0:
                 foundzero = 1
                 zi = i
                 zj = j
                 break
-        if (foundzero):
+        if foundzero:
             break
-    if (foundzero == 0):
+    if foundzero == 0:
         return matrix
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if (matrix[i][j] == 0):
+    for i, mi in enumerate(matrix):
+        for j, mj in enumerate(mi):
+            if mj == 0:
                 matrix[i][zj] = 0
                 matrix[zi][j] = 0
-    for i in range(len(matrix)):
-        if ((matrix[i][zj] == 0) and (i != zi)):
-            for j in range(len(matrix[0])):
-                matrix[i][j] = 0
-    for i in range(len(matrix[0])):
+    for i, mi in enumerate(matrix):
+        if ((mi[zj] == 0) and (i != zi)):
+            for j, mj in enumerate(mi):
+                mj = 0
+    for i, mi in enumerate(matrix[0]):
         if ((matrix[zi][i] == 0) and (i != zj)):
-            for j in range(len(matrix)):
+            for j, mj in enumerate(matrix):
                 matrix[j][i] = 0
-    for i in range(len(matrix)):
-        matrix[i][zj] = 0
-    for i in range(len(matrix[0])):
+    for i, mi in enumerate(matrix):
+        mi[zj] = 0
+    for i, mi in enumerate(matrix[0]):
         matrix[zi][i] = 0
     return matrix
 
 m = int(input("enter m: "))
 n = int(input("enter n: "))
-matrix = []
-for i in range(m):
+matrixinp = []
+for a in range(m):
     t = []
-    for j in range(n):
+    for b in range(n):
         x = int(input())
         t.append(x)
-    matrix.append(t)
+    matrixinp.append(t)
 
-matrixnew = setZeroes(matrix)
+matrixnew = set_zeroes(matrixinp)
 
-for i in range(m):
-    s = ""
-    for j in range(n):
-        s = s + str(matrix[i][j]) + " "
-    print(s)
+for a in range(m):
+    st = ""
+    for b in range(n):
+        st = st + str(matrixnew[a][b]) + " "
+    print(st)
